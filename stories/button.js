d8a1048452a6b13c06889b3ae4c1ca67bd127285
태재영 <tjy970721@gmail.com>
2020-01-23 22:49:13 +0900

    Refactoring code (#1)

---
+++ stories/button.js
@@ -0,0 +1,80 @@
+import spaces from 'one-spaces';
+import { storiesOf } from '@storybook/html';
+
+import '../packages/index.scss';
+
+storiesOf('Button', module)
+  .add('Contained Button', () => `
+    <h1>Basic</h1>
+    <button class="${spaces('mca-button', 'mca-button--contained')}">
+      <span class="mca-button__label">BUTTON</span>
+    </button>
+    <h1>Disabled</h1>
+    <button disabled class="${spaces('mca-button', 'mca-button--contained')}">
+      <span class="mca-button__label">BUTTON</span>
+    </button>
+    <h1>Leading Icon</h1>
+    <button class="${spaces('mca-button', 'mca-button--contained')}">
+      <i class="${spaces('material-icons', 'mca-icon', 'mca-icon--dense')}">grade</i><!--
+   --><span class="mca-button__label">BUTTON</span>
+    </button>
+    <h1>Trailing Icon</h1>
+    <button class="${spaces('mca-button', 'mca-button--contained')}">
+      <span class="mca-button__label">BUTTON</span><!--
+   --><i class="${spaces('material-icons', 'mca-icon', 'mca-icon--dense')}">grade</i>
+    </button>
+  `)
+  .add('Outlined Button', () => `
+    <h1>Basic</h1>
+    <button class="${spaces('mca-button', 'mca-button--outlined')}">
+      <span class="mca-button__label">BUTTON</span>
+    </button>
+    <h1>Disabled</h1>
+    <button disabled class="${spaces('mca-button', 'mca-button--outlined')}">
+      <span class="mca-button__label">BUTTON</span>
+    </button>
+    <h1>Leading Icon</h1>
+    <button class="${spaces('mca-button', 'mca-button--outlined')}">
+      <i class="${spaces('material-icons', 'mca-icon', 'mca-icon--dense')}">grade</i><!--
+   --><span class="mca-button__label">BUTTON</span>
+    </button>
+    <h1>Trailing Icon</h1>
+    <button class="${spaces('mca-button', 'mca-button--outlined')}">
+      <span class="mca-button__label">BUTTON</span><!--
+   --><i class="${spaces('material-icons', 'mca-icon', 'mca-icon--dense')}">grade</i>
+    </button>
+  `)
+  .add('Text Button', () => `
+    <h1>Basic</h1>
+    <button class="${spaces('mca-button', 'mca-button--text')}">
+      <span class="mca-button__label">BUTTON</span>
+    </button>
+    <h1>Disabled</h1>
+    <button disabled class="${spaces('mca-button', 'mca-button--text')}">
+      <span class="mca-button__label">BUTTON</span>
+    </button>
+    <h1>Leading Icon</h1>
+    <button class="${spaces('mca-button', 'mca-button--text')}">
+      <i class="${spaces('material-icons', 'mca-icon', 'mca-icon--dense')}">grade</i><!--
+   --><span class="mca-button__label">BUTTON</span>
+    </button>
+    <h1>Trailing Icon</h1>
+    <button class="${spaces('mca-button', 'mca-button--text')}">
+      <span class="mca-button__label">BUTTON</span><!--
+   --><i class="${spaces('material-icons', 'mca-icon', 'mca-icon--dense')}">grade</i>
+    </button>
+    </section>
+  `)
+  .add('Toggle Button', () => `
+    <section>
+      <button class="${spaces('mca-button', 'mca-button--toggle')}">
+        <i class="${spaces('material-icons', 'mca-icon')}">format_align_justify</i>
+      </button>
+      <button disabled class="${spaces('mca-button', 'mca-button--toggle')}">
+        <i class="${spaces('material-icons', 'mca-icon')}">format_align_left</i>
+      </button>
+      <button class="${spaces('mca-button', 'mca-button--toggle')}">
+        <i class="${spaces('material-icons', 'mca-icon')}">format_align_right</i>
+      </button>
+    </section>
+  `);  
--- stories/icon.js
+++ stories/icon.js
@@ -1,9 +1,9 @@
 import { storiesOf } from '@storybook/html';
 
-import '../../packages/button/button.scss';
+import '../packages/index.scss';
 
 storiesOf('Icon', module)
   .add('Material Icon', () => `
     <i class="material-icons mca-icon">grade</i>
-    <i class="material-icons mca-icon--dense">grade</i>
+    <i class="material-icons mca-icon mca-icon--dense">grade</i>
   `);
