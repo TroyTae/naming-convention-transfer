dac632a105da4cf3d7f8a154497a6f353e40e0a6
태재영 <tjy970721@gmail.com>
2020-01-23 16:33:40 +0900

    split stories

--- stories/button.js
+++ stories/button.js
@@ -1,78 +1,67 @@
+import spaces from 'one-spaces';
 import { storiesOf } from '@storybook/html';
 
 import '../packages/index.scss';
 
-const renderHtml = (id) => (`
-  <article id="${id}">
+storiesOf('Button', module)
+  .add('Contained Button', () => `
     <section>
-      <h1>Text Button</h1>
-      <button class="mca-button--text">
+      <button class="${spaces('mca-button', 'mca-button--contained')}">
         <span class="mca-button__label">BUTTON</span>
       </button>
-      <button class="mca-button--text">
-        <i class="material-icons mca-icon--dense">grade</i><!--
+      <button class="${spaces('mca-button', 'mca-button--contained')}">
+        <i class="${spaces('material-icons', 'mca-icon', 'mca-icon--dense')}">grade</i><!--
      --><span class="mca-button__label">BUTTON</span>
       </button>
-      <button class="mca-button--text">
-        <span class="mca-button__label">BUTTON</span><!--
-     --><i class="material-icons mca-icon--dense">grade</i>
-      </button>  
-    </section>
-    <section>
-      <h1>Outlined Button</h1>
-      <button class="mca-button--outlined">
-        <span class="mca-button__label">BUTTON</span>
-      </button>
-      <button class="mca-button--outlined">
-        <i class="material-icons mca-icon--dense">grade</i><!--
-     --><span class="mca-button__label">BUTTON</span>
-      </button>
-      <button class="mca-button--outlined">
-        <span class="mca-button__label">BUTTON</span><!--
-     --><i class="material-icons mca-icon--dense">grade</i>
-      </button>
-    </section>
-    <section>
-      <h1>Contained Button</h1>
-      <button class="mca-button--contained">
-        <span class="mca-button__label">BUTTON</span>
-      </button>
-      <button class="mca-button--contained">
-        <i class="material-icons mca-icon--dense">grade</i><!--
-     --><span class="mca-button__label">BUTTON</span>
-      </button>
-      <button class="mca-button--contained">
+      <button class="${spaces('mca-button', 'mca-button--contained')}">
         <span class="mca-button__label">BUTTON</span><!--
      --><i class="material-icons mca-icon--dense">grade</i>
       </button>   
     </section>
+  `)
+  .add('Outlined Button', () => `
     <section>
-      <h1>Toggle Button</h1>
-      <button class="mca-button--toggle">
-        <i class="material-icons mca-icon">format_align_justify</i>
+      <button class="${spaces('mca-button', 'mca-button--outlined')}">
+        <span class="mca-button__label">BUTTON</span>
       </button>
-      <button class="mca-button--toggle">
-        <i class="material-icons mca-icon">format_align_left</i>
+      <button class="${spaces('mca-button', 'mca-button--outlined')}">
+        <i class="material-icons mca-icon--dense">grade</i><!--
+     --><span class="mca-button__label">BUTTON</span>
       </button>
-      <button class="mca-button--toggle">
-        <i class="material-icons mca-icon">format_align_right</i>
+      <button class="${spaces('mca-button', 'mca-button--outlined')}">
+        <span class="mca-button__label">BUTTON</span><!--
+     --><i class="material-icons mca-icon--dense">grade</i>
       </button>
     </section>
+  `)
+  .add('Text Button', () => `
     <section>
-      <h1>FAB</h1>
-      <button class="mca-button--fab--regular">
-        <i class="material-icons mca-icon">add</i>
+      <button class="${spaces('mca-button', 'mca-button--text')}">
+        <span class="mca-button__label">BUTTON</span>
       </button>
-      <button class="mca-button--fab--dense">
-        <i class="material-icons mca-icon--dense">add</i>
+      <button class="${spaces('mca-button', 'mca-button--text')}">
+        <i class="material-icons mca-icon--dense">grade</i><!--
+     --><span class="mca-button__label">BUTTON</span>
       </button>
-      <button class="mca-button--fab--extend">
-        <i class="material-icons mca-icon">add</i><!--
-     --><span class="mca-button__label">CREATE</span>
+      <button class="${spaces('mca-button', 'mca-button--text')}">
+        <span class="mca-button__label">BUTTON</span><!--
+     --><i class="material-icons mca-icon--dense">grade</i>
       </button>
     </section>
-  </article>
-`);
-
-storiesOf('Button', module)
-  .add('Default Theme', () => renderHtml('default-button'));
+  `)
+  .add('Toggle Button', () => `
+    <section>
+      <button class="${spaces('mca-button', 'mca-button--toggle')}">
+        <span class="mca-button__label">BUTTON</span>
+      </button>
+      <button class="${spaces('mca-button', 'mca-button--toggle')}">
+        <i class="material-icons mca-icon--dense">grade</i><!--
+     --><span class="mca-button__label">BUTTON</span>
+      </button>
+      <button class="${spaces('mca-button', 'mca-button--toggle')}">
+        <span class="mca-button__label">BUTTON</span><!--
+     --><i class="material-icons mca-icon--dense">grade</i>
+      </button>
+    </section>
+  `)
+  
