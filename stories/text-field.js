6dd9f515a598f173713cbfab80d9dfb5c7304dcd
태재영 <tjy970721@gmail.com>
2020-01-24 18:16:48 +0900

    docs build

--- stories/text-field.js
+++ stories/text-field.js
@@ -4,9 +4,11 @@ import { storiesOf } from '@storybook/html';
 import '../packages/index.scss';
 
 storiesOf('Text Field', module)
-  .add('Contained Button', () => `
-    <h1>Basic</h1>
-    <div class="mca-text-field">
-      <input type="text" class="mca-text-field__input" />
-    </div>
+  .add('Filled Text Field', () => `
+    <article style="width: 280px;">
+      <h1>Basic</h1>
+      <div class="${spaces('mca-text-field', 'mca-text-field--filled')}">
+        <input type="text" class="mca-text-field__input" />
+      </div>
+    </article>
   `);
