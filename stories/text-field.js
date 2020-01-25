57c99224ce3315a26f6d5e58344103bfd2fce66c
태재영 <tjy970721@gmail.com>
2020-01-25 14:22:23 +0900

    split fixed style

--- stories/text-field.js
+++ stories/text-field.js
@@ -7,32 +7,56 @@ storiesOf('Text Field', module)
   .add('Filled Text Field', () => `
     <article style="max-width: 280px;">
       <h1>Basic</h1>
-      <div class="${spaces('mca-text-field', 'mca-text-field--filled')}">
-        <input type="text" class="mca-text-field__input" />
+      <div class="mca-text-field">
+        <input
+          type="text"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--filled')}"
+        />
       </div>
       <h1>Placeholder</h1>
-      <div class="${spaces('mca-text-field', 'mca-text-field--filled')}">
-        <input type="text" placeholder="Write text in here" class="mca-text-field__input" />
+      <div class="mca-text-field">
+        <input
+          type="text"
+          placeholder="Write text in here"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--filled')}"
+        />
       </div>
       <h1>Disabled</h1>
-      <div class="${spaces('mca-text-field', 'mca-text-field--filled')}">
-        <input disabled type="text" placeholder="You cannot write in here" class="mca-text-field__input" />
+      <div class="mca-text-field">
+        <input
+          disabled
+          type="text"
+          placeholder="You cannot write in here"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--filled')}"
+        />
       </div>
     </article>
   `)
   .add('Outlined Text Field', () => `
     <article style="max-width: 280px;">
       <h1>Basic</h1>
-      <div class="${spaces('mca-text-field', 'mca-text-field--outlined')}">
-        <input type="text" class="mca-text-field__input" />
+      <div class="mca-text-field">
+        <input
+          type="text"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--outlined')}"
+        />
       </div>
       <h1>Placeholder</h1>
-      <div class="${spaces('mca-text-field', 'mca-text-field--outlined')}">
-        <input type="text" placeholder="Write text in here" class="mca-text-field__input" />
+      <div class="mca-text-field">
+        <input
+          type="text"
+          placeholder="Write text in here"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--outlined')}"
+        />
       </div>
       <h1>Disabled</h1>
-      <div class="${spaces('mca-text-field', 'mca-text-field--outlined')}">
-        <input disabled type="text" placeholder="You cannot write in here" class="mca-text-field__input" />
+      <div class="mca-text-field">
+        <input
+          disabled
+          type="text"
+          placeholder="You cannot write in here"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--outlined')}"
+        />
       </div>
     </article>
   `);
