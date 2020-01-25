fed4875495e62950ef60dc2438615c9f6c115b9e
태재영 <tjy970721@gmail.com>
2020-01-25 14:25:54 +0900

    Text field (#3)

--- stories/text-field.js
+++ stories/text-field.js
@@ -5,10 +5,58 @@ import '../packages/index.scss';
 
 storiesOf('Text Field', module)
   .add('Filled Text Field', () => `
-    <article style="width: 280px;">
+    <article style="max-width: 280px;">
       <h1>Basic</h1>
-      <div class="${spaces('mca-text-field', 'mca-text-field--filled')}">
-        <input type="text" class="mca-text-field__input" />
+      <div class="mca-text-field">
+        <input
+          type="text"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--filled')}"
+        />
+      </div>
+      <h1>Placeholder</h1>
+      <div class="mca-text-field">
+        <input
+          type="text"
+          placeholder="Write text in here"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--filled')}"
+        />
+      </div>
+      <h1>Disabled</h1>
+      <div class="mca-text-field">
+        <input
+          disabled
+          type="text"
+          placeholder="You cannot write in here"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--filled')}"
+        />
+      </div>
+    </article>
+  `)
+  .add('Outlined Text Field', () => `
+    <article style="max-width: 280px;">
+      <h1>Basic</h1>
+      <div class="mca-text-field">
+        <input
+          type="text"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--outlined')}"
+        />
+      </div>
+      <h1>Placeholder</h1>
+      <div class="mca-text-field">
+        <input
+          type="text"
+          placeholder="Write text in here"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--outlined')}"
+        />
+      </div>
+      <h1>Disabled</h1>
+      <div class="mca-text-field">
+        <input
+          disabled
+          type="text"
+          placeholder="You cannot write in here"
+          class="${spaces('mca-text-field__input', 'mca-text-field__input--outlined')}"
+        />
       </div>
     </article>
   `);
