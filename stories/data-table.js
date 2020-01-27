dc24f548d97a877b6f2e7b7ebae35fb60d7081f0
태재영 <tjy970721@gmail.com>
2020-01-27 23:00:22 +0900

    border/text

--- stories/data-table.js
+++ stories/data-table.js
@@ -9,18 +9,18 @@ storiesOf('Data Table', module)
     <table class="mca-data-table">
       <thead class="mca-data-table__head">
         <tr class="mca-data-table__head__row">
-          <td class="mca-data-table__head__row__column">title1</td>
-          <td class="mca-data-table__head__row__column">title2</td>
+          <td class="mca-data-table__head__row__column">Title1</td>
+          <td class="mca-data-table__head__row__column">Title2</td>
         </tr>
       </thead>
       <tbody class="mca-data-table__body">
         <tr class="mca-data-table__body__row">
-          <td class="mca-data-table__body__row__column">content1</td>
-          <td class="mca-data-table__body__row__column">content2</td>
+          <td class="mca-data-table__body__row__column">Content1</td>
+          <td class="mca-data-table__body__row__column">Content2</td>
         </tr>
         <tr class="mca-data-table__body__row">
-          <td class="mca-data-table__body__row__column">content3</td>
-          <td class="mca-data-table__body__row__column">content4</td>
+          <td class="mca-data-table__body__row__column">Content3</td>
+          <td class="mca-data-table__body__row__column">Content4</td>
         </tr>
       </tbody>
     </table>
