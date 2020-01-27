230d29a5297fac16ef7987e54467604d945eed74
태재영 <tjy970721@gmail.com>
2020-01-27 14:56:58 +0900

    disabled example

--- stories/selection-control.js
+++ stories/selection-control.js
@@ -32,6 +32,25 @@ storiesOf('Selection Control', module)
       </div>
       <span class="middle">Checkbox3</span>
     </label>
+    <h1>Disabled</h1>
+    <label>
+      <div class="mca-selection-control middle">
+        <input disabled checked type="checkbox" class="mca-selection-control__input" />
+        <div class="mca-selection-control__checkbox">
+          <i class="material-icons mca-icon mca-icon--dense">check</i>
+        </div>
+      </div>
+      <span class="middle">Checked</span>
+    </label>
+    <label>
+      <div class="mca-selection-control middle">
+        <input disabled type="checkbox" class="mca-selection-control__input" />
+        <div class="mca-selection-control__checkbox">
+          <i class="material-icons mca-icon mca-icon--dense">check</i>
+        </div>
+      </div>
+      <span class="middle">Unchecked</span>
+    </label>
   `)
   .add('Radio Button', () => `
     <h1>Basic</h1>
@@ -55,5 +74,20 @@ storiesOf('Selection Control', module)
         <div class="mca-selection-control__radio"></div>
       </div>
       <span class="middle">Radio button3</span>
+    </label>
+    <h1>Disabled</h1>
+    <label>
+      <div class="mca-selection-control middle">
+        <input disabled checked type="radio" name="disabled" value="checked" class="mca-selection-control__input" />
+        <div class="mca-selection-control__radio"></div>
+      </div>
+      <span class="middle">Checked</span>
+    </label>
+    <label>
+      <div class="mca-selection-control middle">
+        <input disabled type="radio" name="disabled" value="unchecked" class="mca-selection-control__input" />
+        <div class="mca-selection-control__radio"></div>
+      </div>
+      <span class="middle">Unchecked</span>
     </label>
   `);
