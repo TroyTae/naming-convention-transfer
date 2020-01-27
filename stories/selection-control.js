96cddb04651b927415687d726107eccfc1af3b66
태재영 <tjy970721@gmail.com>
2020-01-27 13:22:45 +0900

    selection control width label

--- stories/selection-control.js
+++ stories/selection-control.js
@@ -5,15 +5,49 @@ import '../packages/index.scss';
 storiesOf('Selection Control', module)
   .add('Checkbox', () => `
     <h1>Basic</h1>
-    <div class="mca-selection-control">
-      <input type="checkbox" class="mca-selection-control__input" />
-      <div class="mca-selection-control__checkbox"></div>
-    </div>
+    <label>
+      <div class="mca-selection-control">
+        <input type="checkbox" class="mca-selection-control__input" />
+        <div class="mca-selection-control__checkbox"></div>
+      </div>
+      <span>Checkbox1</span>
+    </label>
+    <label>
+      <div class="mca-selection-control">
+        <input type="checkbox" class="mca-selection-control__input" />
+        <div class="mca-selection-control__checkbox"></div>
+      </div>
+      <span>Checkbox2</span>
+    </label>
+    <label>
+      <div class="mca-selection-control">
+        <input type="checkbox" class="mca-selection-control__input" />
+        <div class="mca-selection-control__checkbox"></div>
+      </div>
+      <span>Checkbox3</span>
+    </label>
   `)
   .add('Radio Button', () => `
     <h1>Basic</h1>
-    <div class="mca-selection-control">
-      <input type="radio" class="mca-selection-control__input" />
-      <div class="mca-selection-control__radio"></div>
-    </div>
+    <label>
+      <div class="mca-selection-control">
+        <input type="radio" name="basic" class="mca-selection-control__input" />
+        <div class="mca-selection-control__radio"></div>
+      </div>
+      <span>Radio button1</span>
+    </label>
+    <label>
+      <div class="mca-selection-control">
+        <input type="radio" name="basic" class="mca-selection-control__input" />
+        <div class="mca-selection-control__radio"></div>
+      </div>
+      <span>Radio button2</span>
+    </label>
+    <label>
+      <div class="mca-selection-control">
+        <input type="radio" name="basic" class="mca-selection-control__input" />
+        <div class="mca-selection-control__radio"></div>
+      </div>
+      <span>Radio button3</span>
+    </label>
   `);
