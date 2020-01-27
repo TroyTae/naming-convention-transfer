80e0bdf0899777b56ecf298fe669b7d08397c5fe
태재영 <tjy970721@gmail.com>
2020-01-27 14:52:15 +0900

    px -> rem

--- stories/selection-control.js
+++ stories/selection-control.js
@@ -37,21 +37,21 @@ storiesOf('Selection Control', module)
     <h1>Basic</h1>
     <label>
       <div class="mca-selection-control middle">
-        <input type="radio" name="basic" class="mca-selection-control__input" />
+        <input type="radio" name="basic" value="1" class="mca-selection-control__input" />
         <div class="mca-selection-control__radio"></div>
       </div>
       <span class="middle">Radio button1</span>
     </label>
     <label>
       <div class="mca-selection-control middle">
-        <input type="radio" name="basic" class="mca-selection-control__input" />
+        <input type="radio" name="basic" value="2" class="mca-selection-control__input" />
         <div class="mca-selection-control__radio"></div>
       </div>
       <span class="middle">Radio button2</span>
     </label>
     <label>
       <div class="mca-selection-control middle">
-        <input type="radio" name="basic" class="mca-selection-control__input" />
+        <input type="radio" name="basic" value="3" class="mca-selection-control__input" />
         <div class="mca-selection-control__radio"></div>
       </div>
       <span class="middle">Radio button3</span>
