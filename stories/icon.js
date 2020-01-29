9b709464c129808013249c06ee6c71ac8465b15f
troy <tjy970721@gmail.com>
2020-01-29 10:32:49 +0900

    init project

--- stories/icon.js
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/html';
-
-import './index.scss';
-
-storiesOf('Icon', module)
-  .add('Material Icon', () => `
-    <i class="material-icons mca-icon">grade</i>
-    <i class="material-icons mca-icon mca-icon--dense">grade</i>
-  `);
