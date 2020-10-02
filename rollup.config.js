9401b41de873d64042cb5a8430b0f955e184d43d
태재영 <tjy970721@gmail.com>
2020-10-02 19:09:28 +0900

    remove troyjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,28 @@
 const pkg = require('./package.json');
-const fs = require('fs-extra');
-const {createUniversalConfigs} = require('troyjs/rollup');
+const fs  = require('fs-extra');
+const {terser} = require('rollup-plugin-terser');
+const typescript = require('rollup-plugin-typescript2');
+const {kebabToPascal} = require('naming-convention-transfer');
+
+const input = 'src/index.ts';
+
+function createConfig({ pkg, input, format, file }) {
+  return {	
+    input,
+    output: {
+      file,
+      format,
+      exports: 'auto',
+      name: kebabToPascal(pkg.name)
+    },	
+    plugins: [typescript(), terser()]
+  };	
+}
 
 fs.emptyDirSync('./dist');
 
-module.exports = createUniversalConfigs({
-  input: 'src/index.ts',
-  package: pkg
-});
+module.exports = [
+  createConfig({ pkg, input, format: 'cjs', file: pkg.main }),
+  createConfig({ pkg, input, format: 'esm', file: pkg.module }),
+  createConfig({ pkg, input, format: 'iife', file: `dist/${pkg.name}.js` })
+];
